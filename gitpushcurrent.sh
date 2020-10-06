ba60eacf6cb9a07bca2269afa8249269ac9e9ccc
hkailee <leehongkai@gmail.com>
2020-10-06 11:06:58 +0800

    first commit

---
+++ gitpushcurrent.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#git rm -r --cached .
+#git filter-branch --tree-filter 'rm -rf deep-learning/projects/other_mini_projects/cycleGAN/summer2winter_yosemite.zip' HEAD
+#git filter-branch --tree-filter 'rm -rf deep-learning/projects/other_mini_projects/DCGAN_SVHN/data/train_32x32.mat' HEAD
+git add .
+git commit -m "docs"
+git push
