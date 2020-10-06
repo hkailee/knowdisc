5724d8e56d31bce7f31a8e1b6b264909b6ddc19d
hkailee <leehongkai@gmail.com>
2020-10-06 17:41:53 +0800

    docs

--- gitpushcurrent.sh
+++ gitpushcurrent.sh
@@ -5,3 +5,12 @@
 git add .
 git commit -m "docs"
 git push
+
+# from github initiation
+#echo "# knowdisc" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/hkailee/knowdisc.git
+#git push -u origin main
